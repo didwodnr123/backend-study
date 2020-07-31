c4649a17fb3c6bcd1e87484f6293779d7e3f7b2d
jaeukyang <didwodnr123@gmail.com>
2020-07-31 19:43:50 +0900

    생활코딩: NodeJS 공부 시작

--- server/basic.js
+++ server/basic.js
@@ -6,10 +6,13 @@ var person = {
 
 console.log(person.name, person.age)
 
-//JSON 배열 생성하긱
+// function getInfo(idx){
+//     console.log(people[idx].info)
+// }
+
+//JSON 배열 생성하기
 var people = [
     {name: "Jaeuk", age: 25},
     {name: "Yongwook", age: 24}
 ]
-
-console.log(people[0].name)
+console.log(people)
--- vanillaJS/index.js
+++ vanillaJS/index.js
@@ -5,11 +5,11 @@ const title = document.getElementById("title")
 const CLICKED_CLASS = "clicked"
 
 function handleClick(){
-    const currentClass = title.className
-    if(currentClass !== CLICKED_CLASS){
-        title.className = CLICKED_CLASS
-    } else{
-        title.className = ""
+    const hasClass = title.classList.contains(CLICKED_CLASS)
+    if(!hasClass){
+        title.classList.add(CLICKED_CLASS); 
+    } else {
+        title.classList.remove(CLICKED_CLASS)
     }
 }
 
