8d71710eebceef6c36c936bbc0aeaf1b56b645cd
jaeukyang <didwodnr123@gmail.com>
2020-08-11 18:33:35 +0900

    Foldering

---
+++ NodeJS/main.js
@@ -0,0 +1,91 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+ 
+var app = http.createServer(function(request,response){
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query;
+    var pathName = url.parse(_url, true).pathname;
+    var title = queryData.id;
+    
+    if(pathName === '/'){
+      if(queryData.id === undefined){
+        fs.readdir('./data', function(error, filelist){
+          console.log(filelist)
+          var title = 'Welcome'
+          var description = 'Hello. Node.js'
+          
+          var list = '<ul>'
+
+          var i = 0
+          while(i < filelist.length){
+            list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
+            i = i + 1
+          }
+
+          list = list + '</ul>'
+
+          var template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            ${list}
+            <h2>${title}</h2>
+            <p>${description}</p>
+          </body>
+          </html>
+          `;
+          response.writeHead(200);  
+          response.end(template);
+        })
+        
+    } else {
+      fs.readdir('./data', function(error, filelist){
+        var title = 'Welcome'
+        var description = 'Hello. Node.js'
+        
+        var list = '<ul>'
+
+        var i = 0
+        while(i < filelist.length){
+          list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
+          i = i + 1
+        }
+
+        list = list + '</ul>'
+
+          fs.readFile(`./data/${queryData.id}`, 'utf8', function(err, description){
+            var title = queryData.id
+            var template = `
+            <!doctype html>
+            <html>
+            <head>
+              <title>WEB1 - ${title}</title>
+              <meta charset="utf-8">
+            </head>
+            <body>
+              <h1><a href="/">WEB</a></h1>
+              ${list}
+              <h2>${title}</h2>
+              <p>${description}</p>
+            </body>
+            </html>
+            `;
+            response.writeHead(200);  
+            response.end(template);
+          })
+        })
+      }
+
+    } else {
+      response.writeHead(404);
+      response.end('Not Found');
+    }
+    
+});
+app.listen(3000);
