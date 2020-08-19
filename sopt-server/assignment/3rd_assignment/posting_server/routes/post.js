c74b232f199d8a674dc455f88d07d3b2fe5eab35
jaeukyang <didwodnr123@gmail.com>
2020-08-19 22:24:50 +0900

    Update: posting server

--- sopt-server/assignment/3rd_assignment/posting_server/routes/post.js
+++ sopt-server/assignment/3rd_assignment/posting_server/routes/post.js
@@ -5,6 +5,7 @@ const statusCode = require('../modules/statusCode');
 const resMessage = require('../modules/responseMessage');
 const util = require('../modules/util');
 
+// 게시글 고유 id 값 조회
 router.get('/:id', async (req, res) => {
     const id = req.params.id;
     const post = Posts.filter(post => post.id == id)[0];
@@ -23,6 +24,36 @@ router.get('/:id', async (req, res) => {
     // 성공 - login success와 함께 user Id 반환
     res.status(statusCode.OK)
         .send(util.success(statusCode.OK, resMessage.READ_PROFILE_SUCCESS, dto));
+})
+
+// 게시글 생성
+router.post('/', async (req, res) => {
+    const {
+        id,
+        title,
+        text
+    } = req.body
+    
+    if(!id || !title || !text){
+        res.status(statusCode.BAD_REQUEST).send(statusCode.BAD_REQUEST, resMessage.NULL_VALUE)
+        return
+    }
+
+    if(Posts.filter(post => post.id == id).length > 0){
+        res.status(statusCode.BAD_REQUEST).send(statusCode.BAD_REQUEST, resMessage.ALREADY_ID)
+        return
+    }
+
+    Posts.push({
+        id,
+        title,
+        text
+    })
+    console.log(Posts)
+
+    res.status(statusCode.OK)
+        .send(util.success(statusCode.OK, resMessage.POSTING_SUCCESS, {postId: id}));
+
 })
 
 module.exports = router
