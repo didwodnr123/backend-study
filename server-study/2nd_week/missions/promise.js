95cfc6179c6bbfe41caf09d6dd3ba6309845abe2
jaeukyang <didwodnr123@gmail.com>
2020-09-03 00:52:27 +0900

    Update: missions/promise.js

--- server-study/2nd_week/missions/promise.js
+++ server-study/2nd_week/missions/promise.js
@@ -1,16 +1,32 @@
+// let isMomHappy = false;
+// let phone = {
+//     brand: 'iPhone',
+//     color: 'black'
+// };
+
+// var willIGetNewPhone = new Promise(
+//     function (resolve, reject) {
+//         if (isMomHappy) {
+//             resolve(console.log(phone)); // fulfilled
+//         } else {
+//             var reason = new Error('mom is not happy');
+//             reject(reason); // reject
+//         }
+//     }
+// );
+
 let isMomHappy = false;
 let phone = {
-    brand: 'iPhone',
+    brand: 'Samsung',
     color: 'black'
-};
+}
 
-var willIGetNewPhone = new Promise(
-    function (resolve, reject) {
-        if (isMomHappy) {
-            resolve(console.log(phone)); // fulfilled
-        } else {
-            var reason = new Error('mom is not happy');
-            reject(reason); // reject
-        }
+var willIGetNewPhone = new Promise((resolve, reject) => {
+    if(isMomHappy){
+        resolve(console.log(phone));
+    }else{
+        //reject(console.error());
+        reject(console.log(new Error('Mom is not happy')))
     }
-);
+})
+
